845da42e5673e83a56febf68afa000f6d65c6985
unknown <edercruz.x@gmail.com>
2025-01-31 10:36:51 -0300

    rota get com paramentro e rota delete com parametro

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express'   // importar o express pro prjecto
 
 
 const app = express()    // criar instancia do express e armazenar na const app
+
+app.use(express.json()) // indicar ao express ler o body com json
 // mock
 const selecoes = [
   {id: 1, selecao:"Brasil", grupo:"G"},
@@ -19,6 +21,12 @@ app.get('/', (req, res) => {  // toda vez ao fazer uma requiscao http precisa de
 app.get("/selecoes",(req, res)=> {
   res.status(200).send(selecoes)//enviaar uma resposta)
 })
+app.post("/selecoes",(req, res)=> {
+selecoes.push(req.body)     //pegar o corpo da  requisicao
+res.status(202).send("")
 
+
+
+})
 
 export default app
